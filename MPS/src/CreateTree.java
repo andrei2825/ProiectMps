3df125243f6ba1b6fc84481fb4a56ccc85b14480
iulianamihaela <mihaela.iuliana870@gmail.com>
2023-01-09 00:55:53 +0200

    Added JavaDocs to CreateTree.java

--- MPS/src/CreateTree.java
+++ MPS/src/CreateTree.java
@@ -1,5 +1,11 @@
 import java.util.*;
 public class CreateTree {
+    /**
+     * Selects the specific distinct thresholds
+     * @param data
+     * @param indexes
+     * @return
+     */
     public ArrayList<Double> selectThresholds(ArrayList<Double> data, ArrayList<Integer> indexes) {
         int numOfThresholds = data.size();
         ArrayList<Double> thresholds = new ArrayList<>();
@@ -13,12 +19,16 @@ public class CreateTree {
         return thresholds;
     }
 
+    /**
+     * Creates a node
+     * @param thresholds list of thresholds
+     * @param eq
+     * @return
+     */
     public double createNode(ArrayList<Double> thresholds, int eq) {
         double node= 0;
         NodeEq nodeEq = new NodeEq();
         node = nodeEq.pick(eq, thresholds);
         return node;
     }
-
-
 }
